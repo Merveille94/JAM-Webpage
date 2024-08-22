8e09c576514d0460cbc79bdd0cbc48f1def45589
Merveille94 <emanueleguehi@gmail.com>
2024-08-22 04:59:39 +0000

    Commit to JAM page5 Finished

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -27,22 +27,26 @@ const Hero = () => {
     return (
         <section className='w-full h-3/4 xl:h-screen relative'>
             <img className='h-full w-full object-cover' alt='background image brown' src={bgImage} />
-            <div className='absolute w-full h-full top-0 left-0 bg-gradient-to-t from-zinc-800 to-gray-900/60'></div>
+            <div className='absolute w-full h-full top-0 left-0 bg-gradient-to-t from-zinc-900 via-gray-900/60'></div>
             <div className='absolute inset-0 flex items-center justify-center'>
-                <img src={bgImage2} alt='background image' className='h-full w-auto m-auto left-0 right-0 z-10'/>
-                    <div className='absolute top-1/2 transform -translate-y-1/2 left-[14%] z-20 text-left left-text transition-transform duration-1000 ease-out'>
-                        <p className='text-white text-3xl md:text-5xl lg:text-7xl xl:text-9xl font-bold'>Japhet</p>
-                    </div>
-                    <div className='absolute bottom-1/4 transform -translate-y-1/2 right-[14%] z-5 text-right right-text transition-transform duration-1000 ease-out'>
-                        <p className='text-white text-3xl md:text-5xl lg:text-7xl xl:text-9xl font-bold'>Adjetey</p>
-                    </div>
+                <div className='relative right-0 left-0 inset-0 w-[50%] md:w-[43%] '><img src={bgImage2} alt='background image' className='h-full w-auto m-auto left-0 right-0 z-10'/></div>
+                <div className='absolute w-full h-full top-0 left-0 bg-gradient-to-t from-zinc-900'></div>
+                <div className='absolute top-1/2 transform -translate-y-1/2 left-[14%] z-20 text-left left-text transition-transform duration-1000 ease-out'>
+                    <p className='text-white text-3xl md:text-5xl lg:text-7xl xl:text-9xl font-bold'>Japhet</p>
+                </div>
+                <div
+                    className='absolute bottom-1/4 transform -translate-y-1/2 right-[14%] z-5 text-right right-text transition-transform duration-1000 ease-out'>
+                    <p className='text-white text-3xl md:text-5xl lg:text-7xl xl:text-9xl font-bold'>Adjetey</p>
+                </div>
                 <div className='absolute bottom-0 right-0 p-4 z-10 text-slate-100'>
                     <div className='flex items-center space-x-4'>
-                        <button className='border border-slate-200 transition-all duration-700 ease-in-out hover:border-transparent rounded-full p-2 md:p-3'>
+                        <button
+                            className='border border-slate-200 transition-all duration-700 ease-in-out hover:border-transparent rounded-full p-2 md:p-3'>
                             <BsFillPlayCircleFill size={30} className='hover:text-red-700'/>
                         </button>
                         <div className='hidden md:block bg-gray-800 p-4 rounded-md max-w-xs'>
-                            <p className='text-xs md:text-sm '>The most streamed Gospel Artist. New video <strong>Healing Factor</strong>is now on all music platforms</p>
+                            <p className='text-xs md:text-sm '>The most streamed Gospel Artist. New video <strong>Healing
+                                Factor</strong>is now on all music platforms</p>
                         </div>
                     </div>
                 </div>
