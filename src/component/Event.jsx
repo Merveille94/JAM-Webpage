851d25b6a30201f4d21bcf0c513e86843c00edd8
Merveille94 <emanueleguehi@gmail.com>
2024-08-23 08:02:08 +0000

    testimony page done

--- src/component/Event.jsx
+++ src/component/Event.jsx
@@ -7,12 +7,12 @@ const Event = () => {
     return (
         <div className='h-3/4 bg-black w-full flex items-center justify-center py-8 md:py-10 ld:py-12 xl:py-16 px-8 md:px-14'>
             <div className='w-full my-8'>
-                <div className='container mx-auto flex justify-between items-center'>
-                    <div className='flex flex-col items-center justify-center w-[70%]'>
-                        <div className='container mx-auto text-3xl md:text-5xl lg:text-6xl font-bold bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent'>{TITLE.gig}</div>
-                        <div className='container flex items-center text-slate-200 mx-auto pt-2'><p className='max-w-xs'>Follow our ongoing concerts all over the world in 2022.</p></div>
+                <div className='container mx-auto flex justify-center md:justify-between items-center'>
+                    <div className='flex flex-col items-center justify-center w-full md:w-[70%]'>
+                        <div className='container text-center md:text-start mx-auto text-3xl md:text-5xl lg:text-6xl font-bold bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent'>{TITLE.gig}</div>
+                        <div className='container text-center md:text-start flex items-center text-slate-200 mx-auto pt-2'><p className='max-w-full md:max-w-xs'>Follow our ongoing concerts all over the world in 2022.</p></div>
                     </div>
-                    <div className='p-2 hover:border-transparent border border-violet-700 rounded text-slate-200'><Link to={'/events'}>More Gigs</Link></div>
+                    <div className='hidden md:flex p-2 hover:border-transparent border border-violet-700 rounded text-slate-200'><Link to={'/events'}>More Gigs</Link></div>
                 </div>
                 <div className='container mx-auto p-4 md:p-8 lg:p-12 xl:p-14 grid grid-cols-1 grid-rows-5 gap-y-10 my-8 text-slate-200'>
                     <div className='flex items-center justify-between container my-4 mx-auto border-b border-slate-500 pb-1'>
