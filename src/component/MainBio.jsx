a7f59ecff356491c6969917eebe68dbca3ef511f
Merveille94 <emanueleguehi@gmail.com>
2024-08-27 14:38:56 +0000

    navbar

--- src/component/MainBio.jsx
+++ src/component/MainBio.jsx
@@ -1,50 +1,31 @@
 import { useState } from 'react';
-import { MAINBIOTEXT, TITLE } from '../constant';
-import { Link } from 'react-router-dom';
+import { MAINBIOTEXT, TITLE} from '../constant';
+import {Link} from 'react-router-dom';
 import { FaArrowLeft } from 'react-icons/fa';
 import bgImage from "../assets/image_8.jpg";
-import { FaFacebook, FaInstagram, FaXTwitter } from "react-icons/fa6";
-import { GrYoutube } from "react-icons/gr";
-import { motion } from 'framer-motion';
-import { useInView } from 'react-intersection-observer';
+import {FaFacebook, FaInstagram, FaXTwitter} from "react-icons/fa6";
+import {GrYoutube} from "react-icons/gr";
 
 const MainBio = () => {
     const [showFullText, setShowFullText] = useState(false);
-    const { ref, inView } = useInView({
-        triggerOnce: true,
-        threshold: 0.1,
-    });
 
     const toggleText = () => setShowFullText(!showFullText);
 
     return (
-        <section ref={ref} className='relative w-full min-h-screen'>
+        <section className='relative w-full min-h-screen'>
             {/* Background Image */}
-            <motion.img
+            <img
                 className='absolute inset-0 h-full w-full object-cover'
                 alt='background image'
                 src={bgImage}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 1 }}
             />
 
             {/* Dark Overlay */}
-            <motion.div
-                className="absolute inset-0 bg-gradient-to-t from-zinc-900 via-zinc-900 bg-opacity-70"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 0.7 }}
-                transition={{ duration: 1 }}
-            />
+            <div className="absolute inset-0 bg-gradient-to-t from-zinc-900 via-zinc-900 bg-opacity-70"></div>
 
             {/* Content Container */}
             <div className='relative z-10 w-full py-8 md:py-10 lg:py-12 xl:py-16 px-8 md:px-14'>
-                <motion.div
-                    className='flex justify-center items-center mx-auto my-6 xl:my-12'
-                    initial={{ y: -20, opacity: 0 }}
-                    animate={inView ? { y: 0, opacity: 1 } : {}}
-                    transition={{ duration: 1 }}
-                >
+                <div className='flex justify-center items-center mx-auto my-6 xl:my-12'>
                     <div className='flex flex-col items-center justify-center pb-1'>
                         <div className='text-4xl md:text-5xl lg:text-7xl xl:text-9xl font-bold bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent'>
                             {TITLE.about}
@@ -52,23 +33,13 @@ const MainBio = () => {
                         <div className='p-2 text-slate-100 text-xs md:text-sm lg:text-md xl:text-lg'>
                             {TITLE.story}
                         </div>
-                        <Link to={'/'}>
-                            <FaArrowLeft
-                                size={30}
-                                className="text-3xl md:text-5xl text-slate-200 hover:text-violet-400 cursor-pointer transition-all duration-300 mt-4"
-                            />
-                        </Link>
+                        <Link to={'/'}><FaArrowLeft size={30} className=" text-3xl md:text-5xl text-slate-200 hover:text-violet-400 cursor-pointer transition-all duration-300 mt-4 " /></Link>
                     </div>
-                </motion.div>
+                </div>
 
-                <div className='w-full md:w-[95%] lg:w-[95%] xl:w-[95%] container mx-auto p-2 md:p-4 lg:p-8 xl:p-10 my-6 text-slate-200'>
+                <div className=' w-full md:w-[95%] lg:w-[95%] xl:w-[95%] container mx-auto p-2 md:p-4 lg:p-8 xl:p-10 my-6 text-slate-200'>
                     {/* Text Section with Floating Image */}
-                    <motion.div
-                        className="relative bg-purple-900 bg-opacity-60 text-white rounded-lg p-6 md:p-10 shadow-lg floating-effect"
-                        initial={{ scale: 0.8, opacity: 0 }}
-                        animate={inView ? { scale: 1, opacity: 1 } : {}}
-                        transition={{ duration: 0.8 }}
-                    >
+                    <div className="relative bg-purple-900 bg-opacity-60 text-white rounded-lg p-6 md:p-10 shadow-lg floating-effect">
                         <h1 className="text-3xl md:text-4xl font-bold mb-4">Japhet Adjetey</h1>
                         <div className={`text-lg md:text-xl text-justify leading-relaxed ${showFullText ? '' : 'max-h-96 lg:max-h-svh overflow-hidden'}`}>
                             {MAINBIOTEXT.map((paragraph, index) => (
@@ -77,21 +48,18 @@ const MainBio = () => {
                                 </p>
                             ))}
                         </div>
-                        <button
-                            onClick={toggleText}
-                            className="lg:hidden mt-3 px-4 py-2 bg-violet-700 hover:bg-violet-500 text-sm md:text-lg text-white rounded-lg transition-all duration-300"
-                        >
+                        <button onClick={toggleText} className="lg:hidden mt-3 px-4 py-2 bg-violet-700 hover:bg-violet-500 text-sm md:text-lg text-white rounded-lg transition-all duration-300">
                             {showFullText ? 'Show Less' : 'Read More'}
                         </button>
 
                         {/* Social Media Icons */}
                         <div className="hidden mt-4 md:flex space-x-6 justify-center md:justify-start">
-                            <FaInstagram size={20} className='hover:text-orange-500' />
-                            <FaXTwitter size={20} className='hover:text-black' />
-                            <FaFacebook size={20} className='hover:text-blue-700' />
-                            <GrYoutube size={20} className='hover:text-red-500' />
+                            <FaInstagram size={20} className='hover:text-orange-500'/>
+                            <FaXTwitter size={20} className='hover:text-black'/>
+                            <FaFacebook size={20} className='hover:text-blue-700'/>
+                            <GrYoutube size={20} className='hover:text-red-500'/>
                         </div>
-                    </motion.div>
+                    </div>
                 </div>
             </div>
         </section>
